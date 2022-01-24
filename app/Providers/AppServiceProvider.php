1917687aaddd86c9556bbec2c5408ed38bcfe7c3
Dusan Djordjevic <dussann1997@live.com>
2022-01-24 18:54:26 +0100

    Added vue and vite directive for blade

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Helpers\ViteAssets;
+use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +25,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Blade::directive('vite', [ViteAssets::class, 'generate']);
     }
 }
