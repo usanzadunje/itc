76102548abdb2e801be838b0e3633f087c3a96b5
Dusan Djordjevic <dussann1997@live.com>
2022-01-26 17:58:28 +0100

    Added time <=> project relations

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Project extends Model
 {
@@ -16,5 +17,9 @@ class Project extends Model
 
     public function user(): BelongsTo {
         return $this->belongsTo(User::class);
+    }
+
+    public function projects(): HasMany {
+        return $this->hasMany(Time::class);
     }
 }
--- app/Models/Time.php
+++ app/Models/Time.php
@@ -4,8 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Time extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'time_spent',
+    ];
+
+    public function project(): BelongsTo {
+        return $this->belongsTo(Project::class);
+    }
 }
